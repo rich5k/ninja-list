054d75b90c498cad829f2ad14e10576b6a73e2c0
rich5k <richardanatsui@gmail.com>
2021-05-20 17:54:36 +0100

    initial

---
+++ comps/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return ( 
+        <footer>
+            Copyright 2021 A-Minded Tech
+        </footer>
+     );
+}
+ 
+export default Footer;
---
+++ pages/about.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+const About = () => {
+    return ( 
+        <>
+            <Head>
+            <title>Ninja List | About</title>
+            <meta name="keywords" content="ninjas"/>
+            </Head>
+            <div>
+                <h1>About</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente fugiat ipsam beatae ea ad distinctio ab accusantium suscipit adipisci assumenda voluptatem molestiae quae explicabo, amet consequatur enim repellat dolorem veniam.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente fugiat ipsam beatae ea ad distinctio ab accusantium suscipit adipisci assumenda voluptatem molestiae quae explicabo, amet consequatur enim repellat dolorem veniam.</p>
+            </div>
+        </>
+     );
+}
+ 
+export default About;
